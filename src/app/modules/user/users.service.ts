94aab02ba00392d52d35f1c8cf4c480a4e34270a
charles JABBOUR GEDEON <c.jg@zerocodeautomation.io>
2020-09-30 14:31:28 +0200

    UsersService patched

--- src/app/modules/user/users.service.ts
+++ src/app/modules/user/users.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 import { User } from 'src/app/models/user.model';
 import { ApiService } from './api.service';
 import { AbstractUserService } from './users.service.abstract';
@@ -7,7 +8,7 @@ import { AbstractUserService } from './users.service.abstract';
 @Injectable()
 export class UsersService extends AbstractUserService {
 
-  // private users = new Array<User>();
+  users = new BehaviorSubject<User[]>(new Array<User>());
 
   constructor(
     private api: ApiService,
@@ -15,18 +16,22 @@ export class UsersService extends AbstractUserService {
   ) {
     super();
     console.log('UsersService');
-
+    this.users.next([
+      new User(0, 'john@ib.fr'),
+      new User(1, 'luke@ib.fr'),
+      new User(2, 'erza@ib.fr')
+    ]);
   }
 
   getUsers(): Array<User> {
     return [];
   }
 
-  addUser(user: User) {
-    // this.users.push(user);
-  }
-
   refresh() {
     throw new Error('Method not implemented.');
+  }
+
+  addUser(user: User) {
+    // this.users.push(user);
   }
 }
